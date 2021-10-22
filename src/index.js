b40d72fa99db60187eedf62e1a0aee20b667bd1f
Pema Lama <pema.srp72@gmail.com>
2021-10-22 06:25:07 -0400

    commit tree 5ae5e45e2e3c8961b7607811d9c4878144af6fcb

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 const init = () => {
-  
-}
+const inputForm = document.querySelector("form");
+inputForm.addEventListener('submit',(e)=> {
+  e.preventDefault();
+  const searchedId = document.querySelector("input#searchByID");
+    console.log(searchedId.value);
 
+fetch(`http://localhost:3000/movies/${searchedId.value}`)
+.then(res => res.json())
+.then(data => {
+  console.log(data)
+  const title = document.querySelector("h4");
+  const summary = document.querySelector("p");
+
+  title.innerText = data.title;
+  summary.innerText = data.summary;
+  
+})
+})
+}
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
+
+
+
+
